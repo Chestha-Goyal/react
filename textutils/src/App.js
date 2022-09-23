57c41ecc9755082b4851141aabbd5993c5c6d0cc
Chestha-Goyal <chesthagoyal0000@gmail.com>
2022-09-23 23:54:08 +0530

    react

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -4,20 +4,35 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
   //state variable it tells whether the darkmode is enabled or not 
   const [mode, setMode] = useState('light');
   // state variable dark mode wala
   //  dark hoga to light hoga, light hoga to light hoga
+
+  //alert name ki state 
+  const [alert, setAlert] = useState(null); 
+
+  // alert ko set krga
+  const showAlert = (message, type) => {
+     setAlert({
+      msg: message,
+      type: type
+     })
+  }
+
   const toggleMode = () =>{
     if(mode === 'light'){
     setMode('dark');
     document.body.style.backgroundColor = '#042743';
+    setAlert("Dark mode has been enabled", "success");
     }
     else{
       setMode('light');
       document.body.style.backgroundColor = 'white';
+      setAlert("Light mode has been enabled", "success");
     }
   }
   // setMode = 'light' directly asa nhi kr skte kyuki yeah ik state hai ham statevariable ko directly asa use nhi kr skte
@@ -31,13 +46,14 @@ function App() {
        props are just property isme ham khud components bna kr khud properties dete hai*/}
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     {/* toggleMode ik function higa jo navbar me jyga actual me uoer hoga yeah */}
+    <Alert alert={alert}/>
     <div className="container my-3">
       <TextForm heading="Enter the text" mode={mode}/>
       {/* <About/> */}
     </div>
 
     
-    </>
+    </> 
   );
 }
 

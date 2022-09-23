cf5686b4e27c897314c74701b1810e5653e64777
Chestha-Goyal <chesthagoyal0000@gmail.com>
2022-09-24 01:38:37 +0530

    react

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,10 +1,16 @@
 // import { useState } from 'react';
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   //state variable it tells whether the darkmode is enabled or not 
@@ -32,7 +38,7 @@ function App() {
     document.body.style.backgroundColor = '#042743';
     showAlert("Dark mode has been enabled", "success");
     document.title = 'TextUtils - Dark Mode';
-    // to attract public 
+    // to attract public title flash
     // setInterval(() => {
     // document.title = 'TextUtils is amazing Mode';
     // }, 2000);
@@ -58,14 +64,22 @@ function App() {
        {/* ham props pass kr rhe hai
        props are read only  yeah text({props.title}) change ho skta hai bs  
        props are just property isme ham khud components bna kr khud properties dete hai*/}
+    <Router>
     <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
     {/* toggleMode ik function higa jo navbar me jyga actual me uoer hoga yeah */}
     <Alert alert={alert}/>
     <div className="container my-3">
-      <TextForm  showAlert={showAlert} heading="Enter the text" mode={mode}/>
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <TextForm  showAlert={showAlert} heading="Enter the text" mode={mode}/>
+          </Route>
+    </Switch>
       {/* <About/> */}
     </div>
-
+    </Router>
     
     </> 
   );

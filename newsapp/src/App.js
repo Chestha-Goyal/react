a68e0780062447f4b921cf8939730479c491ed81
Chestha-Goyal <chesthagoyal0000@gmail.com>
2022-09-26 11:48:56 +0530

    react

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import NewsItem from './components/NewsItem';
 
 // function base me directlty return krte hai and class based me render me return krte hai
 
 export default class App extends Component {
   render() {
+    // render ik life cycle method hai jab react ik component ko run krti hai to kuch series of method run hote hai
     return (
       <div>
-        Hello my first class based Component
+        <Navbar/>
+        <News/>
+        <NewsItem/>
       </div>
     )
   }

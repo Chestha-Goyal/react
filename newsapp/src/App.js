6f5abb2de286d712853bac0e8cf40e640fe43999
Chestha-Goyal <chesthagoyal0000@gmail.com>
2022-09-26 10:57:30 +0530

    react

---
+++ newsapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+// function base me directlty return krte hai and class based me render me return krte hai
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        Hello my first class based Component
+      </div>
+    )
+  }
+}

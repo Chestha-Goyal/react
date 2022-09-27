b0e32b9661df2068390b42de0ff7e94829336338
Chestha-Goyal <chesthagoyal0000@gmail.com>
2022-09-27 23:52:41 +0530

    newsapp

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -32,13 +32,27 @@ export class News extends Component {
 ]
   constructor() {
     super(); 
-    console.log("Hello I am a constructor from new component");
+    // console.log("Hello I am a constructor from new component");
     this.state = {
       articles: this.articles,
       loading: false
     }
  }
+//yeah ik life cycle hota hai 
+// render ke bd run hota hai
+
+// async function apni body ke andr kuch function ke resolve hona ka await kr skta hai
+async componentDidMount(){
+  console.log("cdm");
+  let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c4ba04d1a84e4bae9c49f950721a3ab1"
+  let data = await fetch(url);
+  let parsedData = await data.json()
+  console.log(parsedData);
+  this.setState({articles: parsedData.articles})
+ }
+
   render() {
+    console.log("render")
     return (
       <div className="container my-4">
         <h2>NewsMonkey - Top Headlines</h2>
